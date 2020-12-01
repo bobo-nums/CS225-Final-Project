95f765fa5f3cffd57316326b5342852247c59d82
Bowen Xiao <bowenx2@linux-a2.ews.illinois.edu>
2020-11-30 23:25:34 -0600

    cleaned up some files

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
 #include <iostream>
 #include <vector>
 #include "readFromFile.hpp"
+#include "graph.h"
 
 using namespace std;
-
-// int main(int argc, const char * argv[]) {
-
-// 	std::cout << "Filename: " << argv[1] << std::endl;
-	
-// 	// convert file to string representation
-// 	std::cout << "String Representation:" << std::endl;
-// 	std::cout << file_to_string(argv[1]) << std::endl;
-	
-// 	// convert file to vector representation
-// 	std::cout << "Vector Representation:" << std::endl;
-// 	std::vector<std::string> vectorRepr = file_to_vector(argv[1]);
-// 	for (auto word : vectorRepr) {
-// 		std::cout << word << std::endl;
-// 	}	
-// }
-#include "graph.h"
 
 int main(int argc, const char* argv[]){
 	vector<string> out;
@@ -27,9 +11,6 @@ int main(int argc, const char* argv[]){
 	if(argc >= 1){
 		out = file_to_vector(argv[1]);
 		egoNode = egoNode.substr(egoNode.find("/") + 1, egoNode.size() - egoNode.find(".") - 1);
-
-		// for(unsigned i = 0; i < out.size(); i++)
-		// 	cout << out[i] << " ";
 	}
 
 	Graph G(false, true);
@@ -37,5 +18,4 @@ int main(int argc, const char* argv[]){
 
 	G.DFS(egoNode);
 	//G.print();
-	
 }
